0ba168ab934e8a001b529d3d225b9de5065d1c75
heogangsan1 <gjrkdtks123@gmail.com>
2022-10-09 16:07:28 +0900

    백준 제목 변경

--- bookjunone/src/bookjunone/baekjoon2739.java
+++ bookjunone/src/bookjunone/baekjoon2739.java
@@ -6,52 +6,16 @@ public class baekjoon2739 {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int numberOne = sc.nextInt();
-		int numberTwo = sc.nextInt();
-		int numberThree = sc.nextInt();
-		int result = 0;
+		int number = sc.nextInt();
 		
-		if(numberOne == numberTwo && numberOne == numberThree) {
-			result += 10000 + (numberOne * 1000);
-			System.out.println(result);
-		}else if(numberOne == numberTwo){
-			result += 1000 + (numberOne * 100);
-			System.out.println(result);
-		}else if(numberTwo == numberThree) {
-			result += 1000 + (numberTwo * 100);
-			System.out.println(result);
-		}else if(numberOne == numberThree) {
-			result += 1000 + (numberThree * 100);
-			System.out.println(result);
+		
+		
+		
+		for (int i = 1; i < 10; i++) {
 			
-		}else {
-			if (numberOne != numberTwo && numberTwo != numberThree && numberOne != numberThree) {
-				// 만약 a > b 라면
-				if (numberOne > numberTwo) {
-					// c > a > b 라면
-					if (numberThree > numberOne) {
-						result = numberThree;
-					} 
-					// a > (b, c)
-					else {
-						result = numberOne;
-					}
-				}
-				// b > a 라면	
-				else {
-					// c > b > a 라면
-					if (numberThree > numberTwo) {
-						result = numberThree;
-					}
-					// b > (a, c)
-					else {
-						result = numberTwo;
-					}
-				}
-				System.out.println(result * 100);
-			}
+			System.out.println(number + " * " + i + " = "  + (number * i));
 		}
-		
+
 	}
 
 }
